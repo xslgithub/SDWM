f9e322705f2c89ebe66a0cc0626da7e47beb2853
bobo <495399597@qq.com>
2015-10-26 15:38:21 +0800

    解决scrollView滚动问题

---
+++ 闪电外卖/闪电外卖/WMHomeScrollView.h
@@ -0,0 +1,13 @@
+//
+//  WMHomeScrollView.h
+//  闪电外卖
+//
+//  Created by 朱波 on 15/10/26.
+//  Copyright © 2015年 WM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMHomeScrollView : UIScrollView
+
+@end
---
+++ 闪电外卖/闪电外卖/WMHomeScrollView.m
@@ -0,0 +1,14 @@
+//
+//  WMHomeScrollView.m
+//  闪电外卖
+//
+//  Created by 朱波 on 15/10/26.
+//  Copyright © 2015年 WM. All rights reserved.
+//
+
+#import "WMHomeScrollView.h"
+
+@implementation WMHomeScrollView
+
+
+@end
